32e44689a05bd4838ad2746ae7b2e5fd1a06823b
HafsanaSharafudheen <111694967+HafsanaSharafudheen@users.noreply.github.com>
2023-08-17 00:24:02 +0530

    personal

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,18 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+
+    var recipient ="hafsanasharafudheen@gmail.com";
+    var subject = encodeURIComponent("Enquiry request By : "+document.getElementById("FullName").value);
+    var message = encodeURIComponent(document.getElementById("textmessage").value);
+    var mailtoLink ="https://mail.google.com/mail/?view=cm&fs=1&to="+recipient+"&su="+subject+"&body="+message;
+    document.getElementById("email-link").href = mailtoLink;
+    document.getElementById("email-link").click();
+
+  });
+  
+
+
