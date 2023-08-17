36fc55b410d60c1ffce17d01e92133494f8d38b1
HafsanaSharafudheen <111694967+HafsanaSharafudheen@users.noreply.github.com>
2023-08-17 12:11:00 +0530

    css changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -26,16 +25,13 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 
-document.getElementById("contact-form").addEventListener("submit", function(event) {
+document.getElementById("contact-form").addEventListener("submit", function (event) {
 
-    var recipient ="hafsanasharafudheen@gmail.com";
-    var subject = encodeURIComponent("Enquiry request By : "+document.getElementById("FullName").value);
+    var recipient = "hafsanasharafudheen@gmail.com";
+    var subject = encodeURIComponent("Enquiry request By : " + document.getElementById("FullName").value);
     var message = encodeURIComponent(document.getElementById("textmessage").value);
-    var mailtoLink ="https://mail.google.com/mail/?view=cm&fs=1&to="+recipient+"&su="+subject+"&body="+message;
+    var mailtoLink = "https://mail.google.com/mail/?view=cm&fs=1&to=" + recipient + "&su=" + subject + "&body=" + message;
     document.getElementById("email-link").href = mailtoLink;
     document.getElementById("email-link").click();
 
-  });
-  
-
-
+});
